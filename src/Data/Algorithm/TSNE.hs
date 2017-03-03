1616bae2edc3b33da1d293fb77dfcdc0e37a50fb
robagar <robagar@localhost>
2017-03-03 10:30:41 +0000

    tsne -> tsne3D

--- src/Data/Algorithm/TSNE.hs
+++ src/Data/Algorithm/TSNE.hs
@@ -1,5 +1,5 @@
 module Data.Algorithm.TSNE ( 
-        tsne,
+        tsne3D,
         TSNEOptions(..),
         TSNEOutput3D(..)
     ) where
@@ -12,8 +12,8 @@ import Data.Algorithm.TSNE.Internals
 import Data.Algorithm.TSNE.Utils
 
 
-tsne :: TSNEOptions -> TSNEInput -> IO [TSNEOutput3D]
-tsne opts vs = do
+tsne3D :: TSNEOptions -> TSNEInput -> IO [TSNEOutput3D]
+tsne3D opts vs = do
     st <- initState $ length vs
     let ps = neighbourProbabilities opts vs
     return $ [output3D ps st] ++ (execWriter $ evalStateT (runTSNE opts vs ps) st)
